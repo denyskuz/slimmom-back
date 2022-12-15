7919fba3ad5a318402538d00eacef2a9d8c42bc0
Denys Kuz <dkuz@gomage.com>
2022-12-15 18:57:11 +0200

    Init

---
+++ app.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const logger = require('morgan');
+const cors = require('cors');
+const path = require('path');
+
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
+
+require('dotenv').config();
+
+
+const app = express()
+const formatsLogger = process.env.NODE_ENV === 'development' ? 'dev' : 'short'
+
+app.use(logger(formatsLogger))
+app.use(cors())
+app.use(express.json())
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+
+app.use((req, res) => {
+  res.status(400).json({ message: 'Not found' })
+})
+
+app.use((err, req, res, next) => {
+  if (err.name === "ValidationError") {
+    res.status(400).json({
+      message: err.message,
+    });
+  }
+   if (err.status) {
+    return res.status(err.status).json({
+      message: err.message,
+    });
+   }
+  
+ return res.status(500).json({
+    message: "Internal server error",
+ });
+
+})
+
+module.exports = {
+  app
+}
---
+++ readme.md
@@ -0,0 +1,42 @@
+
+# Slim Mom API
+
+
+## Environment Variables
+
+To run this project, you will need to add the following environment variables to your .env file
+
+`NODE_ENV` - developer || production 
+
+`DB_HOST` - path to mongo db 
+
+`SECRET_KEY` - secret key for JWT auth
+
+
+
+
+
+## Installation
+
+Install project with npm
+
+```bash
+  npm install
+```
+Starte project with nodemon
+```bash
+  npm run start:dev
+```
+Starte project in production mode 
+```bash
+  npm run start
+```
+    
+## Running Tests
+
+To run tests, run the following command
+
+```bash
+  npm run test
+```
+
