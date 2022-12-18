4d6220203fb5e2b145abc0a3eb28c9ef4a6e4249
Anton Kutsim <kutsimanton@gmail.com>
2022-12-18 22:07:25 +0200

    route-diary

--- routes/diary.js
+++ routes/diary.js
@@ -9,7 +9,12 @@ router.post("/", async (req, res, next) => {
   // #swagger.description = 'Eнд-поінт на додавання з`їденого продукту у конкретний день'
   // #swagger.responses[400] = { description: 'Bad request' }
   // #swagger.responses[401] = { description: 'Missing header with authorization token' }
-
+  const newProduct = notesService.create(req.body);
+  if (newProduct) {
+    return res.status(201).json({
+      message: "new product created"
+    })
+  }
   return next();
 });
 
@@ -18,7 +23,10 @@ router.get("/", async (req, res, next) => {
   // #swagger.description = 'Eнд-поінт на отримання всієї інформації щодо конкретного дня'
   // #swagger.responses[400] = { description: 'Bad request' }
   // #swagger.responses[401] = { description: 'Missing header with authorization token' }
-
+  const products = await notesService.find();
+  res.json({
+    products
+  })
   return next();
 });
 
@@ -27,7 +35,16 @@ router.delete("/:id", async (req, res, next) => {
   // #swagger.description = 'Eнд-поінт для видалення з`їденого продукту в конкретний день'
   // #swagger.responses[400] = { description: 'Bad request' }
   // #swagger.responses[401] = { description: 'Missing header with authorization token' }
-
+  const { productId } = req.params;
+  const product = notesService.findById({ _id: productId })
+  if (product) {
+    await notesService.findByIdAndDelete({
+      _id: productId
+    });
+  return res.status(200).json({
+      message: "product deleted"
+    });
+  }
   return next();
 });
 
