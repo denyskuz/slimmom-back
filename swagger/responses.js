c20f5cb7895fd6962672428f1b91df5eba857104
PavloLevchenko <teasetrnet@gmail.com>
2022-12-21 20:07:05 +0100

    Updating registration response in swagger

--- swagger/responses.js
+++ swagger/responses.js
@@ -3,7 +3,6 @@ const responses = {
     type: 'object',
     properties: {
       status: { type: 'string', example: 'success' },
-      code: { type: 'number', example: '201' },
       data: {
         type: 'object',
         properties: {
