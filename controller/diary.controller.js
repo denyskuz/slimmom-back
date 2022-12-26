8c778990c442d635966768eccc40488aeea4f636
PavloLevchenko <teasetrnet@gmail.com>
2022-12-26 12:50:30 +0100

    Removing unnecessary try/catch, updating handleErrors

--- controller/diary.controller.js
+++ controller/diary.controller.js
@@ -4,75 +4,63 @@ const { notesService } = require('../service');
 const { noteParamsSchema, noteDateSchema } = require('../validation');
 
 async function addDiaryData(req, res, next) {
-  try {
-    await noteParamsSchema.validateAsync(req.body);
-    const { weight, product, date } = req.body;
-    if (!isValidObjectId(product)) {
-      return next(BadRequest('product is not correct'));
-    }
-    const note = await notesService.create({
-      owner: req.user._id,
-      weight,
-      product,
-      date,
+  await noteParamsSchema.validateAsync(req.body);
+  const { weight, product, date } = req.body;
+  if (!isValidObjectId(product)) {
+    return next(BadRequest('product is not correct'));
+  }
+  const note = await notesService.create({
+    owner: req.user._id,
+    weight,
+    product,
+    date,
+  });
+  if (note) {
+    return res.status(201).json({
+      message: 'new note created',
+      note,
     });
-    if (note) {
-      return res.status(201).json({
-        message: 'new note created',
-        note,
-      });
-    }
-  } catch (error) {
-    next(BadRequest(error.message));
   }
   next();
 }
 
 async function getDiaryByDate(req, res, next) {
-  try {
-    const date = req.params.date;
-    await noteDateSchema.validateAsync(date);
-    const start = new Date(date);
-    start.setHours(0, 0, 0, 0);
-    const end = new Date(date);
-    end.setHours(23, 59, 59, 999);
-    const notes = await notesService
-      .find({
-        owner: req.user._id,
-        date: {
-          $gte: start,
-          $lte: end,
-        },
-      })
-      .populate('product');
+  const date = req.params.date;
+  await noteDateSchema.validateAsync(date);
+  const start = new Date(date);
+  start.setHours(0, 0, 0, 0);
+  const end = new Date(date);
+  end.setHours(23, 59, 59, 999);
+  const notes = await notesService
+    .find({
+      owner: req.user._id,
+      date: {
+        $gte: start,
+        $lte: end,
+      },
+    })
+    .populate('product');
 
-    res.json({
-      notes,
-    });
-  } catch (error) {
-    next(BadRequest(error.message));
-  }
+  res.json({
+    notes,
+  });
   next();
 }
 
 async function removeDiary(req, res, next) {
-  try {
-    const noteId = req.params.noteId;
-    if (!isValidObjectId(noteId)) {
-      return next(BadRequest);
-    }
-    const note = await notesService.findOneAndDelete({
-      owner: req.user._id,
-      _id: noteId,
+  const noteId = req.params.noteId;
+  if (!isValidObjectId(noteId)) {
+    return next(BadRequest);
+  }
+  const note = await notesService.findOneAndDelete({
+    owner: req.user._id,
+    _id: noteId,
+  });
+  if (note) {
+    return res.json({
+      message: 'note deleted',
+      note,
     });
-    if (note) {
-      return res.json({
-        message: 'note deleted',
-        note,
-      });
-    }
-  } catch (error) {
-    next(BadRequest(error.message));
   }
   next();
 }
--- controller/product.controller.js
+++ controller/product.controller.js
@@ -7,77 +7,65 @@ const {
 const { productCalc, pageParams } = require('../helpers');
 
 async function getCalories(req, res, next) {
-  try {
-    await userParamsSchema.validateAsync(req.body);
-    await badProductsQuerySchema.validateAsync(req.query);
-    const { skip, limit } = pageParams(req.query);
+  await userParamsSchema.validateAsync(req.body);
+  await badProductsQuerySchema.validateAsync(req.query);
+  const { skip, limit } = pageParams(req.query);
 
-    const products = await productsService
-      .find({
-        [`groupBloodNotAllowed.${req.body.bloodType}`]: true,
+  const products = await productsService
+    .find({
+      [`groupBloodNotAllowed.${req.body.bloodType}`]: true,
+    })
+    .skip(skip)
+    .limit(limit);
+  const kCal = productCalc(req.body);
+  const user =
+    req.user &&
+    (await usersService
+      .findByIdAndUpdate(req.user._id, req.body, {
+        new: true,
+        runValidators: true,
       })
-      .skip(skip)
-      .limit(limit);
-    const kCal = productCalc(req.body);
-    const user =
-      req.user &&
-      (await usersService
-        .findByIdAndUpdate(req.user._id, req.body, {
-          new: true,
-          runValidators: true,
-        })
-        .lean());
-    return res.json({
-      message: user ? `private ${user.name} parameters updated` : 'public',
-      kCal,
-      products,
-    });
-  } catch (error) {
-    next(error);
-  }
+      .lean());
+  return res.json({
+    message: user ? `private ${user.name} parameters updated` : 'public',
+    kCal,
+    products,
+  });
 }
 
 async function getCategories(req, res, next) {
-  try {
-    await userParamsSchema.validateAsync(req.body);
+  await userParamsSchema.validateAsync(req.body);
 
-    const categories = await productsService
-      .aggregate()
-      .match({ [`groupBloodNotAllowed.${req.body.bloodType}`]: true })
-      .project({ categories: 1 })
-      .unwind('$categories')
-      .group({ _id: '$categories' })
-      .group({ _id: 'categories', titles: { $push: '$_id' } });
+  const categories = await productsService
+    .aggregate()
+    .match({ [`groupBloodNotAllowed.${req.body.bloodType}`]: true })
+    .project({ categories: 1 })
+    .unwind('$categories')
+    .group({ _id: '$categories' })
+    .group({ _id: 'categories', titles: { $push: '$_id' } });
 
-    return res.json({
-      titles: categories[0].titles,
-    });
-  } catch (error) {
-    next(error);
-  }
+  return res.json({
+    titles: categories[0].titles,
+  });
 }
 
 async function getProducts(req, res, next) {
-  try {
-    await productsQuerySchema.validateAsync(req.query);
-    const { title, category } = req.query;
-    const { skip, limit } = pageParams(req.query);
-    const products = await productsService
-      .find({
-        $or: [
-          { 'title.ru': { $regex: '^' + title, $options: 'i' } },
-          { 'title.ua': { $regex: '^' + title, $options: 'i' } },
-          { categories: { $regex: '^' + category, $options: 'i' } },
-        ],
-      })
-      .skip(skip)
-      .limit(limit);
-    return res.json({
-      products,
-    });
-  } catch (error) {
-    next(error);
-  }
+  await productsQuerySchema.validateAsync(req.query);
+  const { title, category } = req.query;
+  const { skip, limit } = pageParams(req.query);
+  const products = await productsService
+    .find({
+      $or: [
+        { 'title.ru': { $regex: '^' + title, $options: 'i' } },
+        { 'title.ua': { $regex: '^' + title, $options: 'i' } },
+        { categories: { $regex: '^' + category, $options: 'i' } },
+      ],
+    })
+    .skip(skip)
+    .limit(limit);
+  return res.json({
+    products,
+  });
 }
 
 module.exports = {
