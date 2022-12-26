3e8a6b9bebf2eb153c0b960117bd357e3613f2f5
PavloLevchenko <teasetrnet@gmail.com>
2022-12-26 18:28:08 +0100

    Removing date restriction

--- validation/diary.js
+++ validation/diary.js
@@ -5,7 +5,6 @@ const noteParamsSchema = Joi.object({
   product: Joi.string().required().example('639aa3fb5e433813b2d2a1d8'),
   date: Joi.date()
     .iso()
-    .greater(new Date().toDateString())
     .required()
     .example('2022-12-21'),
 }).required();
